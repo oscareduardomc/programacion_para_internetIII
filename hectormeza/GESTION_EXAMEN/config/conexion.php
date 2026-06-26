1e5b90ba45d20ab11b742c668af46261f2200248
chavez39-meza <emezachavez39@gmail.com>
2026-06-25 20:25:39 -0600

    Crear conexion.php

--- hectormeza/GESTION_EXAMEN/config/conexion.php
+++ hectormeza/GESTION_EXAMEN/config/conexion.php
@@ -1,1 +1,15 @@
+<?php
+
+    $host = "localhost";
+    $db   = "gestion";
+    $user = "root";
+    $pass = "";
+
+    try {
+        $conexion = new PDO("mysql:host=$host;dbname=$db;charset=utf8", $user, $pass);
+        $conexion->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+    } catch (PDOException $error) {
+        die("Error de conexion: " . $error->getMessage());
+    }
+?>
 
